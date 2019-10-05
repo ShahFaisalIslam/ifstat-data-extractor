e15f4b326a3d44461264b5203eacc3759a0d60de
Faisal <faisal.islam.ceme@gmail.com>
2019-10-05 14:26:30 +0400

    Stores data in files named after the devices

--- extract.c
+++ extract.c
@@ -1,6 +1,7 @@
 //This program will extract all data from the ifstat command and save them
-//Current version: 1.5
+//Current version: 1.6
 //History;
+//-->1.6: saves the data in respective files
 //-->1.5: stores name too
 //-->1.4: extracts the numbers and stores them in float array
 //-->1.3: extracts the numbers and stores them in two string arrays
@@ -25,8 +26,10 @@
 struct timeval start,end;
 
 
-int main()
+int main(int argc, char* argv[])
 {
+gettimeofday(&start,NULL);
+
 	FILE *ifstatCaller; //This will call ifstat command
 
 	char net_name[NAME_SIZE];
@@ -43,8 +46,6 @@ int main()
 	ifstatCaller = popen(command,"r");//calling the command
 
 
-gettimeofday(&start,NULL);
-
 	char* success = fgets(buff,BUFF_SIZE,ifstatCaller);//getting string
 
 	if (success != NULL)
@@ -57,7 +58,7 @@ gettimeofday(&start,NULL);
 		if (name != NULL)
 			{
 				strcpy(net_name,name);
-				printf("Name Extracted:%s\n",net_name);
+				//printf("Name Extracted:%s\n",net_name);
 			}
 		name = NULL;
 		fgets(buff,BUFF_SIZE,ifstatCaller);//ignores the next line that has KB
@@ -74,18 +75,67 @@ gettimeofday(&start,NULL);
 
 			update = strtok(NULL," ");
 			strcpy(inout[1],update);
+//optimization: removal of newline
+			char *newline = strrchr(inout[1],'\n');
+			if (newline != NULL)
+				memcpy(newline,"\0",1);
+			newline = NULL;
+//end of optimization
+
 			inout_n[1] = strtod(update,NULL);
 
-			printf("Update 1: %.2f %.2f\n",inout_n[0],inout_n[1]);//outputting the result
+			//printf("Update 1: %.2f %.2f\n",inout_n[0],inout_n[1]);//outputting the result
 		}
 
 		update = NULL;
 		success = NULL;
+
+
+		char *in_filename;
+		char *out_filename;
+
+		in_filename=calloc(strlen(net_name)+strlen("_in"),sizeof(char));
+		out_filename=calloc(strlen(net_name)+strlen("_out"),sizeof(char));
+
+		strcpy(in_filename,net_name);
+		strcat(in_filename,"_in");
+
+		strcpy(out_filename,net_name);
+		strcat(out_filename,"_out");
+
+
+		FILE *in_file;
+		FILE *out_file;
+
+		in_file=fopen(in_filename,"w");
+		out_file=fopen(out_filename,"w");
+
+		int success_in =fputs(inout[0],in_file);
+		int success_out =fputs(inout[1],out_file);
+
+		if (success_in == 0)
+			printf("Failed to write \"in\"\n");
+		if (success_out == 0)
+			printf("Failed to write \"out\"\n");
+
+
+		free(in_filename);
+		in_filename=NULL;
+
+		free(out_filename);
+		out_filename=NULL;
+
+
+		fclose(in_file);
+		in_file=NULL;
+
+		fclose(out_file);
+		out_file=NULL;
+
 	}
 	else
-		printf("String not obtained\n");
+		printf("Unable to access ifstat\n");
 
-gettimeofday(&end,NULL);
 
 
 	pclose(ifstatCaller);
@@ -95,11 +145,13 @@ gettimeofday(&end,NULL);
 	buff = NULL;
 
 
+gettimeofday(&end,NULL);
+
 {
 	int seconds = end.tv_sec - start.tv_sec;
 	long time = (seconds*1e6) + end.tv_usec - start.tv_usec;
 
-	printf("\n-----Time elapsed in main execution: %ld microseconds-----\n",time);
+	printf("-----Time elapsed in main execution: %ld microseconds-----\n",time);
 }
 
 
